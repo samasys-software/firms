00776c7ed8a1b226cbf8cc58393dfbf1db66c941
kathirvelnatarajan <30907665+kathirvelnatarajan@users.noreply.github.com>
2017-11-29 22:45:18 -0500

    test.sh

--- SamayuSpark_0/SamayuSpark_0/src/main/scripts/test.sh
+++ SamayuSpark_0/SamayuSpark_0/src/main/scripts/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export SPARK_MAJOR_VERSION=2
 echo "Test"
 echo "Tom"
 spark-submit --class TomFile --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 share_file_system/toms.csv /dev/kathirvel/src/toms
@@ -9,37 +10,39 @@ spark-submit --class MonacoFile --deploy-mode client --master yarn samayuspark_0
 fi
 if [ "$?" -eq 0 ];
 then
-echo "cdr"
+echo "cdr" 
 spark-submit --class CdrFile --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 share_file_system/cdr.csv /dev/kathirvel/src/cdr
-fi
-if [ "$?" -eq 0 ];
+fi	        
+if [ "$?" -eq 0 ]; 
 then
-echo "bdr"
+echo "bdr" 
 spark-submit --class BdrFile --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 /dev/kathirvel/src/bdr  /dev/kathirvel/std/bdr.parquet
-fi
+fi		
 if [ "$?" -eq 0 ];
 then
 echo "sdr"
 spark-submit --class SdrFile --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 /dev/kathirvel/src/sdr  /dev/kathirvel/std/sdr.parquet
-fi
+fi			
 if [ "$?" -eq 0 ];
 then
-echo "tomsparquet"
+echo "toms"
 spark-submit --class TomsFileToStd --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 /dev/kathirvel/src/toms  /dev/kathirvel/std/toms.parquet
-fi
+fi	
 if [ "$?" -eq 0 ];
 then
-echo "cdrparquet"
 spark-submit --class CdrFileToStd --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 /dev/kathirvel/src/cdr  /dev/kathirvel/std/cdr.parquet
-fi
+fi	
 if [ "$?" -eq 0 ];
 then
-echo "monacoparquet"
 spark-submit --class MonacoFileToStd --deploy-mode client --master yarn samayuspark_0_2.11-0.1.jar 11/13/2017 /dev/kathirvel/src/monaco  /dev/kathirvel/std/monaco.parquet
-fi
+fi	
 if [ "$?" -eq 0 ];
 then
 echo "passed"
+													
 else
 echo "FAil"
 fi
+cd '/root/scalaproject/'
+./scalaproject_run.sh
+
